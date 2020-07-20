ba2ab09704d4714d6b8be7aa215a2bfc9505aa23
Samuel Christian <smlchrstn31@gmail.com>
2020-07-20 23:57:17 +0200

    change how to module exports

--- src/json_gcounter.js
+++ src/json_gcounter.js
@@ -1,31 +1,32 @@
-"use strict";
+var name = "";
+var data = {[name]: 0};
 
-class JsonGCounter {
-	constructor(replicaName) {
-		this.replicaName = replicaName;
-		this.data = {[replicaName] : 0};
-	}
-
-	inc() {
-		this.data[this.replicaName]++;
-	}
-
-	state() {
-		let sum = 0;
-		let keys = Object.keys(this.data);
-		for (var i = keys.length - 1; i >= 0; i--) {
-			sum += this.data[keys[i]];
-		}
-		return sum;
-	}
-
-	val() {
-		return this.data[this.replicaName];
-	}
-
-	merge(replica) {
-		this.data[replica.key] = replica.val();
-	}
+function inc() {
+	this.data[this.name]++;
 }
 
-module.exports = JsonGCounter;
+function state() {
+	let sum = 0;
+	let keys = Object.keys(data);
+	for (var i = keys.length - 1; i >= 0; i--) {
+		sum += data[keys[i]];
+	}
+	return sum;
+}
+
+function val() {
+	return data[name];
+}
+
+function merge(replica) {
+	this.data[replica.name] = replica.val();
+}
+
+const jsonGCounter = {
+	inc,
+	state,
+	val,
+	merge
+};
+
+module.exports = jsonGCounter;
