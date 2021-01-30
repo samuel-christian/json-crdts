1461a8d285070326e745df9334dbb15dfa853b23
Samuel Christian <smlchrstn31@gmail.com>
2021-01-30 07:38:47 +0100

    fix missing module

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 module.exports.JsonDeltaCrdt = require("./src/json_delta_crdt");
 module.exports.JsonStateCrdt = require("./src/json_state_crdt");
-module.exports.JsonDeltaCrdtServer = require("./src/json_delta_crdt_server");
